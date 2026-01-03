4863c0ccaec49272e025740e7336b931690e7441
JavariaAmir2005 <Javariaamir456@gmail.com>
2026-01-03 15:11:13 +0500

    first commit

--- app/src/main/java/com/example/lifeguard/MainActivity.java
+++ app/src/main/java/com/example/lifeguard/MainActivity.java
@@ -1,24 +1,23 @@
 package com.example.lifeguard;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Use MaterialButton instead of Button
+        MaterialButton btnstart = findViewById(R.id.btnstart);
+
+        btnstart.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class))
+        );
     }
 }
